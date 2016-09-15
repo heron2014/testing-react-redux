9e4f3db18664693ff73720c4be400284088bdfa5
Anita <a.nita@hotmail.co.uk>
2016-09-15 19:17:25 +0100

    refactored CommentBox test to use beforeEach and follow dry principle

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,19 +3,23 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
 
+  let component;
+  // beforeEach is going to be called 3 times, one for each 'it' block
+  // defining component in beforeEach makes our code 'dry'
+  beforeEach(() => {
+    //renderComponent return jquery object that contain react component, this is why we can use jquery methods like find etc
+    component = renderComponent(CommentBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    //renderComponent return jquery object that contain react component, this is why we can use jquery methods like find etc
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 
